ca335bdde945e5a9290a24f95569728351ea04ae
James Hornung <hornung.james@gmail.com>
2016-08-29 10:44:48 -0500

    consistent footer and navbar on all pages, portfolio format complete

--- contact.php
+++ contact.php
@@ -1,3 +1,4 @@
+<!-- PHP Code for E-mail Submission -->
 <?php
     if (isset($_POST["submit"])) {
         $name = $_POST['name'];
@@ -23,7 +24,6 @@
         if (!$_POST['message']) {
             $errMessage = 'Please enter your message';
         }
-        
 
 // If there are no errors, send the email
 if (!$errName && !$errEmail && !$errMessage) {
@@ -36,40 +36,39 @@ if (!$errName && !$errEmail && !$errMessage) {
     }
 ?>
 
-
-
+<!-- BEGIN HTML -->
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Contact</title>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-</head>
-
+    <head>
+        <title>Contact</title>
+        <!-- Compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+    </head>
 <body>
 
-    <!-- Navbar -->
-  <nav class="light-blue lighten-1" role="navigation">
-    <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">James Hornung</a>
-      <ul class="right hide-on-med-and-down">
-        <li><a href="index.html">Main</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="contact.php">Connect</a></li>
-      </ul>
+<!-- Navbar -->
+    <nav class="light-blue lighten-1" role="navigation">
+        <div class="nav-wrapper container"><a id="logo-container" href="#" class="brand-logo">James Hornung</a>
+            <ul class="right hide-on-med-and-down">
+                <li><a href="index.html">Main</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="contact.php">Connect</a></li>
+            </ul>
 
-      <ul id="nav-mobile" class="side-nav">
-        <li><a href="index.html">Main</a></li>
-        <li><a href="about.html">About</a></li>
-        <li><a href="portfolio.html">Portfolio</a></li>
-        <li><a href="connect.php">Connect</a></li>
-      </ul>
-      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
-    </div>
-  </nav>
+            <ul id="nav-mobile" class="side-nav">
+                <li><a href="index.html">Main</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="portfolio.html">Portfolio</a></li>
+                <li><a href="connect.php">Connect</a></li>
+            </ul>
+            <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">menu</i></a>
+        </div>
+    </nav>
 
+<!-- CONTACT FORM -->
     <div class="section no-pad-bot" id="index-banner">
         <div class="container">
             <div class="row">
@@ -98,27 +97,36 @@ if (!$errName && !$errEmail && !$errMessage) {
         </div>
     </div>
 
-    <div class="section">
-      <footer class="page-footer orange">
-        <div class="container">
-          <div class="row">
-            <div class="col l6 s12">
-            </div>
-            <div class="col l3 s12">
-              
-            </div>
-            <div class="col l3 s12">
-              <ul>
-                <li><a class="white-text" href="index.html">Main</a></li>
-                <li><a class="white-text" href="about.html">About</a></li>
-                <li><a class="white-text" href="portfolio.html">Portfolio</a></li>
-                <li><a class="white-text" href="connect.php">Connect</a></li>
-              </ul>
+    <!-- FOOTER -->
+  <div class="section">
+    <footer class="page-footer orange">
+      <div class="container">
+        <div class="row">
+          <!-- Social Media Links -->
+          <div class="col l6 s12">
+            <p class="grey-text text-lighten-4">Connect With Me:</p>
+            <div id="socialLinks">
+              <a href="https://github.com/jameshornung" target="_blank"><img src="images/github.png" class="img-responsive socialLink" alt="Github Logo"></a>
+              <a href="https://www.linkedin.com/in/james-hornung-84526311b" target="_blank"><img src="images/linkedin.png" class="img-responsive socialLink" alt="Linked In Logo"></a>
+              <a href="http://stackoverflow.com/users/6367428/james-hornung?tab=profile" target="_blank"><img src="images/stackOverflow.png" class="img-responsive socialLink" alt="Stack Overflow Logo"></a>
             </div>
           </div>
+          <!-- Empty Div -->
+          <div class="col l3 s12">
+          </div>
+          <!-- Website Links -->
+          <div class="col l3 s12">
+            <ul>
+              <li><a class="white-text" href="index.html">Main</a></li>
+              <li><a class="white-text" href="about.html">About</a></li>
+              <li><a class="white-text" href="portfolio.html">Portfolio</a></li>
+              <li><a class="white-text" href="connect.php">Connect</a></li>
+            </ul>
+          </div>
         </div>
-        
-      </footer>
+      </div>
+    </footer>
+  </div>
 
     <script src="https://code.jquery.com/jquery-3.1.0.min.js"   integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="   crossorigin="anonymous"></script>
     <!-- Compiled and minified JavaScript -->
